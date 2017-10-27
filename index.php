8717b04d72954f93cf84cd69a3b495e0dbdbfaef
IESPINO <iespino69@gmail.com>
2017-10-27 13:30:50 -0500

    Change Title spanish

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <!-- This file has been downloaded from Bootsnipp.com. Enjoy! -->
-    <title>** Login | FixCel **</title>
+    <title> Acceso | FixCel</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
      <link href="css/login_style.css" rel="stylesheet">
